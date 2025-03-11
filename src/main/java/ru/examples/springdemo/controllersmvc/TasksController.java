c69f08fb4cc1c1c6192cc7e98ba2f6ca8e95305e
Nataliya Solina <nataliya.solina@bscmsc.ru>
2025-03-11 20:43:46 +0300

    контроллер ui таск

--- src/main/java/ru/examples/springdemo/controllersmvc/TasksController.java
+++ src/main/java/ru/examples/springdemo/controllersmvc/TasksController.java
@@ -22,30 +22,18 @@ public class TasksController {
     public String getAllTasks(@RequestParam(name = "isDone", required = false) Boolean isDone, Model model) {
         System.out.println("getall=============================");
         List<TaskDto> listTasks = taskService.getTasksByUser(isDone);
-        hiddenTop = false;
         hiddenSingle = true;
         model.addAttribute("listTasks", listTasks);
-        model.addAttribute("hiddenTop", hiddenTop);
         model.addAttribute("hiddenSingle", hiddenSingle);
         return "/task";
     }
 
     @GetMapping("/tasks/{id}")
-    public String getTaskById(
-            @PathVariable Long id, Model model) {
+    public String getTaskById(@PathVariable Long id, Model model) {
         System.out.println("get=============================");
         TaskDto task = taskService.getById(id);
         model.addAttribute("listTasks", task);
-        return "/task";
-    }
-
-    @DeleteMapping("/tasks/{id}")
-    public String deleteTaskById(
-            @PathVariable Long id, Model model) {
-        System.out.println("del=============================" + id);
-        taskService.deleteById(id);
-        model.addAttribute("str", "Deleted");
-        System.out.println("del=============================" + model);
+        model.addAttribute("id", id);
         return "/task";
     }
 
@@ -55,32 +43,82 @@ public class TasksController {
         TaskDto newTask = TaskDto.builder()
                 .userLogin("Nata")
                 .build();
-
         model.addAttribute("newTask", newTask);
         return "/task_new";
     }
 
     @PostMapping(path = "/tasks/new")
-    public String createTaskById(TaskDto taskDto, Model model) {
-
+    public String createTask(TaskDto taskDto, Model model) {
         System.out.println("post=============================" + model);
         System.out.println("post=============================" + taskDto);
         taskService.createTask(taskDto);
         return "redirect:/ui/tasks";
     }
 
-
-    @ExceptionHandler(value = Exception.class)
-    public String exception(Model model) {
-        System.out.println("exception=============================");
-        model.addAttribute("flag", false);
+    @PostMapping("/tasks/{id}:delete")
+    public String deleteTaskById(@PathVariable Long id, Model model) {
+        System.out.println("del=============================" + id);
+        taskService.deleteById(id);
+        hiddenSingle = true;
+        model.addAttribute("hiddenSingle", hiddenSingle);
+        model.addAttribute("id", id);
+        model.addAttribute("str", "Таска с id = " + id + " успешно удалена");
+        System.out.println("del=============================" + model);
         return "/task";
     }
 
-    @GetMapping("/error")
-    public String error(Model model) {
-        System.out.println("error=============================");
+    @GetMapping("/tasks/{id}:put")
+    public String getChangeTask(@PathVariable Long id, Model model) {
+        System.out.println("getchange=============================");
+        System.out.println(id);
+        TaskDto task = taskService.getById(id);
+        TaskDto changeTask = TaskDto.builder()
+                .id(id)
+                .date(task.getDate())
+                .description(task.getDescription())
+                .userLogin(task.getUserLogin())
+                .build();
+        model.addAttribute("changeTask", changeTask);
+        return "/task_change";
+    }
 
+    @PostMapping("/tasks/{id}:put")
+    public String putTaskById(@PathVariable Long id, TaskDto taskDto, Model model) {
+        System.out.println("put=============================" + id);
+        hiddenSingle = true;
+        model.addAttribute("hiddenSingle", hiddenSingle);
+        TaskDto task = taskService.putById(id, taskDto);
+        model.addAttribute("listTasks", task);
+        model.addAttribute("str", "Таска с id = " + id + " успешно исправлена");
+        System.out.println("put=============================" + model);
+        return "/task";
+    }
+
+    @PostMapping("/tasks/{id}:patch")
+    public String patchTaskById(@PathVariable Long id, Model model) {
+        System.out.println("patch=============================" + id);
+        hiddenSingle = true;
+        model.addAttribute("hiddenSingle", hiddenSingle);
+        TaskDto task = taskService.patchById(id);
+        model.addAttribute("listTasks", task);
+        model.addAttribute("str", "Таске с id = " + id + " успешно изменен статус");
+        System.out.println("patch=============================" + model);
+        return "/task";
+    }
+
+
+    @ExceptionHandler(value = Exception.class)
+    public String exception(Exception e, Model model) {
+        System.out.println("exception=============================");
+        String error = e.getMessage();
+        model.addAttribute("error", error);
         return "/error";
     }
+
+//    @GetMapping("/error")
+//    public String error(Model model) {
+//        System.out.println("error=============================");
+//
+//        return "/error";
+//    }
 }
