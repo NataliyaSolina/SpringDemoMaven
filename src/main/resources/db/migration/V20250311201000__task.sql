05b08b962df163b5d88a15b3a678272a630485c5
Nataliya Solina <nataliya.solina@bscmsc.ru>
2025-03-11 20:42:39 +0300

    бд

---
+++ src/main/resources/db/migration/V20250311201000__task.sql
@@ -0,0 +1,20 @@
+INSERT INTO public.task (date, description, user_id, done)
+VALUES ('2025-02-11', 'Закончить курс stepik Spring', 2, true);
+INSERT INTO public.task (date, description, user_id, done)
+VALUES ('2025-02-20', 'Митап по кролику БСЦ', 2, true);
+INSERT INTO public.task (date, description, user_id, done)
+VALUES ('2025-03-12', 'Митап по кролику и кафке Отус', 2, false);
+INSERT INTO public.task (date, description, user_id, done)
+VALUES ('2025-02-12', 'Скомпоновать доклад CI/CD', 2, true);
+INSERT INTO public.task (date, description, user_id, done)
+VALUES ('2025-03-20', 'Закончить курс большой по Spring', 2, false);
+INSERT INTO public.task (date, description, user_id, done)
+VALUES ('2025-03-13', 'Митап по CI/CD', 2, false);
+INSERT INTO public.task (date, description, user_id, done)
+VALUES ('2025-03-12', 'Закинуть в конфлюенс CI/CD jenkins', 2, false);
+INSERT INTO public.task (date, description, user_id, done)
+VALUES ('2025-03-12', 'Доделать речь', 2, false);
+INSERT INTO public.task (date, description, user_id, done)
+VALUES ('2025-02-28', 'Собес мосбиржа', 2, true);
+INSERT INTO public.task (date, description, user_id, done)
+VALUES ('2025-03-06', 'Почти собес)', 2, true);
---
+++ src/main/resources/db/migration/V20250311202500__task.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.task (date, description, user_id, done)
+VALUES ('2025-02-28', 'Дракула', 2, true);
+INSERT INTO public.task (date, description, user_id, done)
+VALUES ('2025-03-20', 'Юнона и Авось', 2, false);
